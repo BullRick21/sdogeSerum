c1c854d9260ff18fe9aa3aedb40421b5649eea0d
Bull Rick <bullst42@gmail.com>
2021-05-08 09:33:24 -0400

    remove other markets from list

---
+++ precache-manifest.dc87567e3383d3e9dae8b9d0c31907d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fec33056323b183571eef640f0630d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ec33325156375ab1df8",
+    "url": "/static/css/2.06ebec9a.chunk.css"
+  },
+  {
+    "revision": "64815efa538bf5cdc13b",
+    "url": "/static/css/main.9ba8d4ac.chunk.css"
+  },
+  {
+    "revision": "7ec33325156375ab1df8",
+    "url": "/static/js/2.bc4297a1.chunk.js"
+  },
+  {
+    "revision": "d2e09aded9686c343e34a065031febca",
+    "url": "/static/js/2.bc4297a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64815efa538bf5cdc13b",
+    "url": "/static/js/main.d004305d.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/static/media/logo.49174c73.svg"
+  }
+]);
